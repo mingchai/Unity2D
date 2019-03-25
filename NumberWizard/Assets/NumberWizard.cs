eac0d4c3f9e79189a162efec3ea3b235b9f6b67a
Ming Chai <ming.chai4@gmail.com>
2019-03-24 19:57:39 -0700

    cleaned up code. Script now posing questions

--- NumberWizard/Assets/NumberWizard.cs
+++ NumberWizard/Assets/NumberWizard.cs
@@ -4,33 +4,36 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    // like JS, we can simply leave a variable undeclared until we're ready.
+        int max = 1001;
+        int min = 9;
+        int guess = 500;
     // Start is called before the first frame update
     void Start()
     {
-        // like JS, we can simply leave a variable undeclared until we're ready.
-        int max = 1001;
-        int min = 9;
-        int average = (max + min)/2;
         Debug.Log("Welcome to Number Wizard!");
-        Debug.Log("Think of a number!");
         // String interpolation can be donw via '$' outside the quotations. Interpolated variables to have curly braces around them.
-        Debug.Log($"The highest number should be {max}");
-        Debug.Log($"The lowest number should be {min}");
-        Debug.Log($"Tell me if your number is higher or lower than {average}");
+        Debug.Log($"Think of a number between {min} and {max}");
+        Debug.Log($"Tell me if your number is higher or lower than {guess}");
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame, so it will then look to identify the below when playing.
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow)){
-            Debug.Log("Up Arrow key was pressed.");
+            min = guess;
+            guess = (max + min)/2;
+            Debug.Log($"Is it higher or lower than {guess}?");
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)){
-            Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            guess = (max + min)/2;
+            Debug.Log($"Is it higher or lower than {guess}?");
         }
         else if(Input.GetKeyDown(KeyCode.Return)){
-            Debug.Log("Return key was pressed.");
+            Debug.Log("The correct number was guessed!");
         }
     }
 }
