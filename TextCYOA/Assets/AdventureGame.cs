22f9da502e56ea6b9c0ab0880e2e3b9e122dbbd6
Ming Chai <ming.chai4@gmail.com>
2019-05-04 19:20:55 -0700

    add states to game object

---
+++ TextCYOA/Assets/AdventureGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    // [SerializeField] makes the variable available in the inspector, so textComponent is then available back in Unity under the game object it's attached to
+    [SerializeField] State startingState;
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        // textComponent.text = "It was a dark and stormy night... That's how it would have started out, ideally. But this was a more idyllic day.";
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TextCYOA/Assets/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "State")]
+// creates a new item in the menu called state which we can then modify below.
+public class State : ScriptableObject
+// structure:
+// access modifier | return type (think primitive values) | ame of object (function name, object name, etc.)
+{
+        [TextArea(14,9)] [SerializeField] string storyText;
+        // TextArea allows us to expand the text box in the inspector
+        [SerializeField] State[] nextStates;
+
+    public string GetStateStory(){
+        return storyText;
+        // When this method is executed, we are returned what's in the textbox of storyText (look in th editor/inspector)
+    }
+
+    public State[] GetNextState(){
+        return nextStates;
+    }
+}
