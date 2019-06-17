d7bd7fece8ad18726145f8b6a7bf018350023b1d
Ming Chai <ming.chai4@gmail.com>
2019-06-17 10:48:27 -0700

    add script to play bounce sound on collision

--- Block Breaker Game/Assets/Scripts/Block.cs
+++ Block Breaker Game/Assets/Scripts/Block.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip blockSound;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(blockSound, Camera.main.transform.position);
+        // We play the audio clip at the Camera's position to ensure the sound is heard clearly by the player as the PlayClipAtPoint method uses a 3D vector. If we set it to the block, we would likely not get the audio quality we are looking for as the block may be far away or too close to the Audio Listener component or we may even hear the sound in the left or right speaker only due to the block's position in space. This is fine for 3D games, but would be especially odd for 2D games like this block breaker project.
         Destroy(gameObject);
     }
 }
