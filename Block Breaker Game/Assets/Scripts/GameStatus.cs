992aa4be0c99a8b05c2475205ebff56ef9e79a05
Ming Chai <ming.chai4@gmail.com>
2020-08-09 21:23:09 -0700

    Add ability to update score through levels

--- Block Breaker Game/Assets/Scripts/GameStatus.cs
+++ Block Breaker Game/Assets/Scripts/GameStatus.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // Config Params
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlock = 10;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // State Variables
     [SerializeField] int currentScore = 0;
 
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if(gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+    
     // Update is called once per frame
     private void Update()
     {
@@ -20,5 +38,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlock;
+        scoreText.text = currentScore.ToString();
     }
 }
