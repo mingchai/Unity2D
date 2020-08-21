85e6f0d157172b2948b019667cb0d996c3c80589
Ming Chai <ming.chai4@gmail.com>
2020-08-20 21:16:57 -0700

    add to VFX to remove particles after 1 sec

--- Block Breaker Game/Assets/Scripts/Block.cs
+++ Block Breaker Game/Assets/Scripts/Block.cs
@@ -39,5 +39,6 @@ public class Block : MonoBehaviour
   private void TriggerSparklesVFX()
   {
     GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+    Destroy(sparkles, 1f);
   }
 }
