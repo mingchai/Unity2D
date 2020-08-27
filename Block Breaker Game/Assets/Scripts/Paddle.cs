f3ccaf68144b46ac385a28b0b0c279d70ab7fcfb
Ming Chai <ming.chai4@gmail.com>
2020-08-27 09:54:46 -0700

    cache ball and session variables

--- Block Breaker Game/Assets/Scripts/Paddle.cs
+++ Block Breaker Game/Assets/Scripts/Paddle.cs
@@ -6,19 +6,35 @@ public class Paddle : MonoBehaviour
 {
     [SerializeField] float screenWidthInUnits= 16f;
     [SerializeField] float minX = 1f, maxX = 15f;
+
+    GameSession gameSession;
+    Ball gameBall;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameSession = FindObjectOfType<GameSession>();
+        gameBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePosX, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosX, minX, maxX);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        // If autoplay is enabled, get the position of the ball, else get the position of the mouse
+        if(gameSession.IsAutoPlayEnabled())
+        {
+            return gameBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
